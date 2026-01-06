f3d84c9d5dc38deaf8b0b1df7013f098b2e4111b
aqgoesdf <aqgoesdf@gmail.com>
2026-01-06 20:39:27 -0300

    modelos para os primeiros posts

--- blogprojec/blog/models.py
+++ blogprojec/blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    titulo = models.CharField(max_length=200)
+    conteudo = models.TextField()
+    data_criacao = models.DateTimeField(auto_now_add=True)
+
+    def __STR__(self):
+        return self.titulo
--- blogprojec/blog/views.py
+++ blogprojec/blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'blog/home.html')
+    posts = Post.objects.all()
+    return render(request, 'blog/home.html', {'post': posts})
