25eeff2a5a102989f6b931ee2b9a0ba2bca3ffb3
aqgoesdf <aqgoesdf@gmail.com>
2026-01-06 21:11:32 -0300

    Funcionalidade dos posts

--- blogprojec/blog/views.py
+++ blogprojec/blog/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
-# Create your views here.
+# Chamada para titulos conteúdo meta descrição.
 
 def home(request):
     posts = Post.objects.all()
-    return render(request, 'blog/home.html', {'post': posts})
+    return render(request, 'blog/home.html', {'posts': posts})
+
+
+# Chamada para os detalhes dos posts
+def post_detalhe(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    return render(request, 'blog/post_detalhe.html', {'post': post})
