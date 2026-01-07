6e1a2397072ca5803b2efe65b1fb5d85c95863b3
aqgoesdf <aqgoesdf@gmail.com>
2026-01-07 15:14:09 -0300

    Resumos dos posts
    Na página home será mostrando somando o resumo dos posts,  ao clicar no leia mais será mostrar o conteúdo dos posts.

--- blogprojec/blog/models.py
+++ blogprojec/blog/models.py
@@ -3,6 +3,10 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     titulo = models.CharField(max_length=200)
+    resumo = models.CharField(
+    max_length=200,
+    default='Resumo não informado'
+)
     conteudo = models.TextField()
     data_criacao = models.DateTimeField(auto_now_add=True)
 
